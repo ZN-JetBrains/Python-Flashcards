d417df7af0f10f5b53dff619ee150644f211b2ca
Zaid Neurothrone <neurothrone@outlook.com>
2021-01-05 11:38:43 +0100

    Complete Stage 2/7: What's on the card

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,4 +1,11 @@
-print("Card:")
-print("purchase")
-print("Definition:")
-print("buy")
+flashcard_dict = {}
+term = input()
+definition = input()
+flashcard_dict[term] = definition
+
+user_guess = input()
+
+if user_guess == flashcard_dict[term]:
+    print("Your answer is right!")
+else:
+    print("Your answer is wrong...")
