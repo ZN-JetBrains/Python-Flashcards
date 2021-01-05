f98e6d27061ba0ab7a17df76c29c374086bc4855
Zaid Neurothrone <neurothrone@outlook.com>
2021-01-05 11:59:47 +0100

    Complete Stage 3/7: Make it your own

--- Flashcards/task/flashcards/flashcards.py
+++ Flashcards/task/flashcards/flashcards.py
@@ -1,11 +1,27 @@
-flashcard_dict = {}
-term = input()
-definition = input()
-flashcard_dict[term] = definition
+def add_cards(a_dict, a_amount_cards):
+    for index in range(a_amount_cards):
+        term = input(f"The term for card #{index + 1}:\n")
+        definition = input(f"The definition for card #{index + 1}:\n")
+        a_dict[term] = definition
 
-user_guess = input()
 
-if user_guess == flashcard_dict[term]:
-    print("Your answer is right!")
-else:
-    print("Your answer is wrong...")
+def play_game(a_dict):
+    for key, value in a_dict.items():
+        print(f"Print the definition of \"{key}\":")
+        user_guess = input()
+
+        if user_guess == value:
+            print("Correct!")
+        else:
+            print(f"Wrong. The right answer is \"{value}\".")
+
+
+def run():
+    flashcard_dict = {}
+    num_cards = int(input("Input the number of cards:\n"))
+    add_cards(flashcard_dict, num_cards)
+    play_game(flashcard_dict)
+
+
+if __name__ == "__main__":
+    run()
